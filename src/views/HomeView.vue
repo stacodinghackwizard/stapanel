9e3990c5779318138763a3e60c6e469b3b419496
Sulaimon Taofeek Ayomide <sulaimontaofeekgithub100@gmail.com>
2023-12-16 23:18:59 +0100

    second commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <button type="button" class="btn btn-primary">Primary Button</button>
+    <h1>This is an about page</h1>
+  </div>
+</template>
